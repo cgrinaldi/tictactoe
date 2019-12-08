21cb49ff5280f2c6bd2e449f057bf3dba04b6622
Chris Rinaldi <cgrinaldi@gmail.com>
2019-12-08 19:18:34 +0000

    Save work for Tic Tac Toe project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,75 @@
+let currentPlayer = 'X'
+let turnNum = 0;
+let isFinished = false;
 
+function performLogic(buttonId, tileId) {
+    if (!isFinished) {
+        // remove button corresponding to buttonId
+        $(buttonId).remove()
+    
+        // change text of div to player value
+        $(tileId).text(currentPlayer);
 
+        // check for winners
+        checkVerticalWins();
+        checkHorizontalWins();
+        checkDiagonalWins();
 
+        // update current player
+        endTurn();
+    }
+    
+}
 
+function isWinner(tileId1, tileId2, tileId3) {
+    let tile1 = $(tileId1).text()
+    let tile2 = $(tileId2).text()
+    let tile3 = $(tileId3).text()
+    if ((tile1 == currentPlayer) && (tile1 == tile2) && (tile1 == tile3)) {
+        return true
+    }
+    return false
+}
 
+function checkVerticalWins() {
+    if (isWinner('#tile1', '#tile4', '#tile7') || 
+        isWinner('#tile2', '#tile5', '#tile8') || 
+        isWinner('#tile3', '#tile6', '#tile9')) {
+            $('h1').text(currentPlayer + 'Wins!')
+            isFinished = true;
+        }
+}
 
+function checkHorizontalWins() {
+    if (isWinner('#tile1', '#tile2', '#tile3') || 
+        isWinner('#tile4', '#tile5', '#tile6') || 
+        isWinner('#tile7', '#tile8', '#tile9')) {
+            $('h1').text(currentPlayer + 'Wins!')
+            isFinished = true;
+        }
+}
+
+function checkDiagonalWins() {
+    if (isWinner('#tile1', '#tile5', '#tile9') ||  
+        isWinner('#tile3', '#tile5', '#tile7')) {
+            $('h1').text(currentPlayer + 'Wins!')
+            isFinished = true;
+        }
+}
+
+function endTurn() {
+    turnNum++;
+    if (currentPlayer == 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X'
+    }
+
+    if (turnNum == 9) {
+        $('h1').text('It is a draw!');
+        isFinished = true;
+    }
+}
 
 $("#button1").click(function() {
     performLogic("#button1","#tile1");
